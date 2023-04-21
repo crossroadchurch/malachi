b9aa61723c21e7bab79bc3e2165b98940981a721
nikukatansa <nikukatansa@users.noreply.github.com>
2023-04-21 21:58:47 +0100

    Multiple monitor detection in startup scripts

--- src/StartChromeWindows.py
+++ src/StartChromeWindows.py
@@ -4,22 +4,35 @@
 """Start the Malachi app and screen windows (Windows OS only)"""
 
 import time
+import ctypes
 import subprocess
 import warnings
 from pywinauto import Application
 
 warnings.simplefilter('ignore', category=UserWarning)
 
-subprocess.Popen(["C:/Program Files (x86)/Google/Chrome/Application/chrome.exe", "http://localhost:8000/app", "--new-window"])
-subprocess.Popen(["C:/Program Files (x86)/Google/Chrome/Application/chrome.exe", "http://localhost:8000/screen", "--new-window"])
-time.sleep(10)  # This may need to be increased if running on a slower system
-app = Application(backend="win32").connect(
-    title="Malachi App - Google Chrome")
-app_window = app.window(title="Malachi App - Google Chrome")
-app_window.move_window(x=0, y=0, width=1366, height=768)
-screen = Application(backend="win32").connect(
-    title="Malachi Screen - Google Chrome")
-screen_window = screen.window(title="Malachi Screen - Google Chrome")
-screen_window.move_window(x=2000, y=0, width=600, height=400)
-screen_window.set_focus()
-screen_window.type_keys("{F11}")
+user32 = ctypes.windll.user32
+# Compare primary screen width to virtual screen width to determine if using multiple monitors
+if user32.GetSystemMetrics(0) == user32.GetSystemMetrics(78):
+    print("Single screen detected - only opening Malachi App")
+    subprocess.Popen(["C:/Program Files (x86)/Google/Chrome/Application/chrome.exe", "http://localhost:8000/app", "--new-window"])
+    time.sleep(10)  # This may need to be increased if running on a slower system
+    app = Application(backend="win32").connect(
+        title="Malachi App - Google Chrome")
+    app_window = app.window(title="Malachi App - Google Chrome")
+    app_window.move_window(x=0, y=0, width=1366, height=768)
+else:
+    print("Multiple screens detected - opening Malachi App and Malachi Screen")
+    subprocess.Popen(["C:/Program Files (x86)/Google/Chrome/Application/chrome.exe", "http://localhost:8000/app", "--new-window"])
+    subprocess.Popen(["C:/Program Files (x86)/Google/Chrome/Application/chrome.exe", "http://localhost:8000/screen", "--new-window"])
+    time.sleep(10)  # This may need to be increased if running on a slower system
+    app = Application(backend="win32").connect(
+        title="Malachi App - Google Chrome")
+    app_window = app.window(title="Malachi App - Google Chrome")
+    app_window.move_window(x=0, y=0, width=1366, height=768)
+    screen = Application(backend="win32").connect(
+        title="Malachi Screen - Google Chrome")
+    screen_window = screen.window(title="Malachi Screen - Google Chrome")
+    screen_window.move_window(x=2000, y=0, width=600, height=400)
+    screen_window.set_focus()
+    screen_window.type_keys("{F11}")
--- src/StartFirefoxWindows.py
+++ src/StartFirefoxWindows.py
@@ -4,24 +4,37 @@
 """Start the Malachi app and screen windows (Windows OS only)"""
 
 import time
+import ctypes
 import subprocess
 import warnings
 from pywinauto import Application, mouse
 
 warnings.simplefilter('ignore', category=UserWarning)
 
-subprocess.Popen(["C:/Program Files/Mozilla Firefox/firefox.exe", "-new-window", "http://localhost:8000/app"])
-subprocess.Popen(["C:/Program Files/Mozilla Firefox/firefox.exe", "-new-window", "http://localhost:8000/screen"])
-time.sleep(10)  # This may need to be increased if running on a slower system
-app = Application(backend="win32").connect(
-    title="Malachi App — Mozilla Firefox")
-app_window = app.window(title="Malachi App — Mozilla Firefox")
-app_window.move_window(x=0, y=0, width=1366, height=768)
-screen = Application(backend="win32").connect(
-    title="Malachi Screen — Mozilla Firefox")
-screen_window = screen.window(title="Malachi Screen — Mozilla Firefox")
-screen_window.move_window(x=2000, y=0, width=600, height=400)
-screen_window.set_focus()
-screen_window.type_keys("{F11}")
-mouse.click(button='left', coords=(2000,500))
-mouse.move(coords=(400,400))
+user32 = ctypes.windll.user32
+# Compare primary screen width to virtual screen width to determine if using multiple monitors
+if user32.GetSystemMetrics(0) == user32.GetSystemMetrics(78):
+    print("Single screen detected - only opening Malachi App")
+    subprocess.Popen(["C:/Program Files/Mozilla Firefox/firefox.exe", "-new-window", "http://localhost:8000/app"])
+    time.sleep(10)  # This may need to be increased if running on a slower system
+    app = Application(backend="win32").connect(
+        title="Malachi App — Mozilla Firefox")
+    app_window = app.window(title="Malachi App — Mozilla Firefox")
+    app_window.move_window(x=0, y=0, width=1366, height=768)
+else:
+    print("Multiple screens detected - opening Malachi App and Malachi Screen")
+    subprocess.Popen(["C:/Program Files/Mozilla Firefox/firefox.exe", "-new-window", "http://localhost:8000/app"])
+    subprocess.Popen(["C:/Program Files/Mozilla Firefox/firefox.exe", "-new-window", "http://localhost:8000/screen"])
+    time.sleep(10)  # This may need to be increased if running on a slower system
+    app = Application(backend="win32").connect(
+        title="Malachi App — Mozilla Firefox")
+    app_window = app.window(title="Malachi App — Mozilla Firefox")
+    app_window.move_window(x=0, y=0, width=1366, height=768)
+    screen = Application(backend="win32").connect(
+        title="Malachi Screen — Mozilla Firefox")
+    screen_window = screen.window(title="Malachi Screen — Mozilla Firefox")
+    screen_window.move_window(x=2000, y=0, width=600, height=400)
+    screen_window.set_focus()
+    screen_window.type_keys("{F11}")
+    mouse.click(button='left', coords=(2000,500))
+    mouse.move(coords=(400,400))
